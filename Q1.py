135c9e59844e60dce1525fd10655df9b5344ccf0
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-04-21 00:31:04 +0530

    Q1 finished

--- Q1.py
+++ Q1.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
 
+
+
 # Load your image
-image = cv2.imread('image 2.jpeg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('Image_01.png', cv2.IMREAD_GRAYSCALE)
 
 # Add Gaussian noise
-noisy_image = image + np.random.normal(0, 25, image.shape).astype(np.uint8)
+noisy_image = image + np.random.normal(0, 20, image.shape).astype(np.uint8)
 
 # Implement Otsu's thresholding
 _, thresholded_image = cv2.threshold(noisy_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 # Define the desired window size (adjust as needed)
-window_width = 300
-window_height = 300
+window_width = 700
+window_height = 700
 
 # Resize the noisy and thresholded images to fit the window
 resized_noisy_image = cv2.resize(noisy_image, (window_width, window_height))
